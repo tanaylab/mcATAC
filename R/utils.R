85b3c62911cee2c6dcbf7af4459b34b86c24901f
aviezerl <aviezer.lifshitz@weizmann.ac.il>
2022-03-01 23:08:35 +0200

    fix typo

--- R/utils.R
+++ R/utils.R
@@ -5,7 +5,7 @@
 `%!in%` <- Negate(`%in%`)
 
 
-check_files_exists <- function(files) {
+check_files_exist <- function(files) {
     for (file in files) {
         if (!file.exists(file) && !grepl("^http", file)) {
             cli_abort("{.file {file}} file doesn't exist.", call = parent.frame(1))
