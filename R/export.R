46dd9ab5ab567bb8a86a132aa711e90402e8ed39
aviezerl <aviezer.lifshitz@weizmann.ac.il>
2022-03-03 15:26:35 +0200

    1. Added import and export from h5ad 2. Added projection using metacell1 3. Started a vignette.

--- R/export.R
+++ R/export.R
@@ -6,9 +6,47 @@
 #'
 #' @return None.
 #'
+#' @examples
+#' \dontrun{
+#' atac_sc <- import_from_10x("pbmc_data")
+#' export_to_h5ad(atac_sc, "pbmc_data/atac_sc.h5ad")
+#' }
+#'
+#' @inheritDotParams anndata::write_h5ad
 #' @export
-export_to_h5ad <- function(object, out_file) {
+export_to_h5ad <- function(object, out_file, ...) {
+    validate_atac_object(object)
 
+    mat <- object$mat
+    mat <- t(mat)
+
+    peaks <- as.data.frame(object$peaks)
+    rownames(peaks) <- peak_names(peaks)
+
+    if (!is.null(object$metadata)) {
+        metadata <- data.frame(rowname = colnames(object$mat)) %>%
+            left_join(metadata) %>%
+            column_to_rownames("rowname")
+    } else {
+        metadata <- NULL
+    }
+
+    cli_ul("Creating an AnnData object")
+    adata <- anndata::AnnData(
+        X = mat,
+        var = peaks,
+        obs = metadata,
+        uns = list(class = class(object)[1])
+    )
+
+    cli_ul("Writing to file")
+    anndata::write_h5ad(
+        adata,
+        out_file,
+        ...
+    )
+
+    cli_alert_success("Successfully exported to {.file {out_file}}")
 }
 
 #' Generate a ucsc genome browser file for each metacell cluster
--- R/import.R
+++ R/import.R
@@ -1,14 +1,56 @@
 
 
-#' Create an ScATAC object from an h5ad file
+#' Create an ScATAC,McATAC object from an h5ad file
 #'
 #' @param file name of an h5ad file with ATAC data
+#' @param class is the file storing ScATAC or McATAC. If NULL - the class would be determined by the 'class' field in the 'uns' part
+#' of the h5ad file, if exists, and otherwise the class would be
+#' McATAC.
 #'
-#' @return an ScATAC object
+#' @return an ScATAC/McATAC object
+#'
+#' @examples
+#' \dontrun{
+#' atac_sc <- import_from_10x("pbmc_data")
+#' export_to_h5ad(atac_sc, "pbmc_data/atac_sc.h5ad")
+#' atac_sc_loaded <- import_from_h5ad("pbmc_data/atac_sc.h5ad")
+#' }
 #'
 #' @export
-import_from_h5ad <- function(file) {
+import_from_h5ad <- function(file, class = NULL) {
+    check_files_exist(file)
+    cli_ul("Reading {.file {file}}")
+    adata <- anndata::read_h5ad(file)
 
+    mat <- t(adata$X)
+
+    peaks <- adata$var
+    metadata <- adata$obs
+    if (ncol(metadata) == 0) {
+        metadata <- NULL
+    }
+
+    if (is.null(class)) {
+        if (!is.null(adata$uns[["class"]])) {
+            class <- adata$uns[["class"]]
+            if (class %!in% c("McATAC", "ScATAC")) {
+                cli_alert_warning("Unknown class name - creating an McATAC object")
+                class <- "McATAC"
+            }
+        } else {
+            class <- "McATAC"
+        }
+    }
+
+    if (class == "McATAC") {
+        res <- McATAC(mat, peaks, metadata)
+        cli_alert_success("Successfully loaded an {.var {class}} object with {.val {ncol(res$mat)}} metacells and {.val {nrow(res$mat)}} ATAC peaks")
+    } else {
+        res <- ScATAC(mat, peaks, metadata)
+        cli_alert_success("Successfully loaded an {.var {class}} object with {.val {ncol(res$mat)}} cells and {.val {nrow(res$mat)}} ATAC peaks")
+    }
+
+    return(res)
 }
 
 #' Create an ScATAC object from 10x directory
@@ -31,11 +73,11 @@ import_from_h5ad <- function(file) {
 import_from_10x <- function(dir = NULL, matrix_fn = file.path(dir, "matrix.mtx"), cells_fn = file.path(dir, "barcodes.tsv"), features_fn = file.path(dir, "features.tsv")) {
     check_files_exist(c(matrix_fn, cells_fn, features_fn))
 
-    cli_alert("Importing matrix")
+    cli_ul("Importing matrix")
     mat <- tgutil::fread_mm(matrix_fn, row.names = features_fn, col.names = cells_fn)
     cli_alert_info("Imported a matrix of {.val {ncol(mat)}} cells and {.val {nrow(mat)}} features")
 
-    cli_alert("Importing features")
+    cli_ul("Importing features")
     features <- tgutil::fread(features_fn, col.names = c("name", "name2", "type", "chrom", "start", "end")) %>%
         as_tibble()
     stopifnot(all(features$name == rownames(mat)))
@@ -48,7 +90,7 @@ import_from_10x <- function(dir = NULL, matrix_fn = file.path(dir, "matrix.mtx")
     cli_alert_info("{.val {nrow(atac_mat)}} ATAC peaks")
 
     res <- ScATAC(atac_mat, atac_peaks)
-    cli_alert_success("Succesfully imported to an ScATAC object with {.val {ncol(atac_mat)}} cells {.val {nrow(atac_mat)}} ATAC peaks")
+    cli_alert_success("Succesfully imported to an ScATAC object with {.val {ncol(atac_mat)}} cells and {.val {nrow(atac_mat)}} ATAC peaks")
 
     return(res)
 }
