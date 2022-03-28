339d04f8a4c9fafc55153ed45a442486937afa44
Aviezer Lifshitz <aviezer.lifshitz@weizmann.ac.il>
2022-03-28 16:55:58 +0300

    Feat@added-reset-paramter-to-ignore_peaks-and-polished-find_blacklist_overlaps (#5)
    * Added a 'reset' paramter to atac_ignore_peaks
    
    * polished find_blacklist_overlap
    
    * Added the script which generated the blacklist intervals
    
    * added tests
    
    * updated vignette
    
    * Fixed typo

--- tests/testthat/test-3-filter_features.R
+++ tests/testthat/test-3-filter_features.R
@@ -34,3 +34,58 @@ test_that("export works when there are ignored peaks", {
     expect_equal(atac_sc_f@peaks, atac_sc1@peaks, ignore_attr = TRUE)
     expect_equal(atac_sc_f@ignore_peaks, atac_sc1@ignore_peaks, ignore_attr = TRUE)
 })
+
+test_that("find_blacklist_overlaps works", {
+    blacklist_overlaps <- find_blacklist_overlaps(atac_sc)
+    atac_sc_f <- atac_ignore_peaks(atac_sc, blacklist_overlaps)
+    misha.ext::gset_genome("hg38")
+    expect_equal(
+        atac_sc@peaks %>%
+            as.data.frame() %>%
+            misha.ext::gintervals.neighbors1("ENCODE.blacklist") %>%
+            filter(dist == 0) %>%
+            anti_join(atac_sc_f@ignore_peaks, by = c("chrom", "start", "end", "peak_name")) %>%
+            nrow(),
+        0
+    )
+
+    expect_equal(
+        atac_sc_f@ignore_peaks,
+        atac_sc@peaks %>%
+            as.data.frame() %>%
+            misha.ext::gintervals.neighbors1("ENCODE.blacklist") %>%
+            filter(dist == 0) %>%
+            select(any_of(colnames(atac_sc_f@peaks))),
+        ignore_attr = TRUE
+    )
+})
+
+test_that("atac_ignore_peaks works twice when reset=FALSE", {
+    set.seed(60427)
+    ignore_peaks1 <- atac_sc@peaks %>% sample_n(100)
+    ignore_peaks2 <- atac_sc@peaks %>%
+        anti_join(ignore_peaks1, by = c("chrom", "start", "end", "peak_name")) %>%
+        sample_n(100)
+
+    atac_sc1 <- atac_ignore_peaks(atac_sc, ignore_peaks1, reset = FALSE)
+    atac_sc2 <- atac_ignore_peaks(atac_sc1, ignore_peaks2, reset = FALSE)
+
+    expect_setequal(atac_sc2@ignore_peaks$peak_name, unique(c(ignore_peaks1$peak_name, ignore_peaks2$peak_name)))
+    expect_setequal(rownames(atac_sc2@ignore_pmat), unique(c(ignore_peaks1$peak_name, ignore_peaks2$peak_name)))
+    expect_true(all(atac_sc2@ignore_peaks$peak_name %!in% atac_sc2@peaks$peak_name))
+    expect_true(all(atac_sc1@ignore_peaks$peak_name %in% atac_sc2@ignore_peaks$peak_name))
+})
+
+test_that("atac_ignore_peaks works twice when reset=TRUE", {
+    set.seed(60427)
+    ignore_peaks1 <- atac_sc@peaks %>% sample_n(100)
+    ignore_peaks2 <- atac_sc@peaks %>%
+        anti_join(ignore_peaks1, by = c("chrom", "start", "end", "peak_name")) %>%
+        sample_n(100)
+
+    atac_sc1 <- atac_ignore_peaks(atac_sc, ignore_peaks1, reset = TRUE)
+    atac_sc2 <- atac_ignore_peaks(atac_sc1, ignore_peaks2, reset = TRUE)
+
+    expect_setequal(atac_sc2@ignore_peaks$peak_name, ignore_peaks2$peak_name)
+    expect_true(!any(atac_sc2@ignore_peaks$peak_name %in% atac_sc1@ignore_peaks$peak_name))
+})
