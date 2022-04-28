88cd898e8e0c702496162f0722553cee13cc7ca9
aviezerl <aviezer.lifshitz@weizmann.ac.il>
2022-04-28 15:48:11 +0300

    removed unused scripts (we can add them afterwards

--- exec/qsub_command.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-unset module;
-outfold=$3;
-base_output=$4;
-cell_names_folder=$5;
-bamfile=$6;
-samtools_path=$7;
-echo $samtools_path 2
-$samtools_path view -bo $outfold/${2%.txt}/${1}.bam -D CB:$cell_names_folder/${2} $bamfile $1;
