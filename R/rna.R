dea093a42a53b212d4c83c654981d373a8bdc0c6
aviezerl <aviezer.lifshitz@weizmann.ac.il>
2022-05-01 15:11:57 +0300

    Added rna marker functions

--- R/rna.R
+++ R/rna.R
@@ -75,6 +75,7 @@ has_rna <- function(mc_atac) {
 #'
 #' @export
 get_rna_matrix <- function(atac_mc, genes = NULL, rm_zeros = TRUE) {
+    validate_atac_object(atac_mc)
     if (!has_rna(atac_mc)) {
         cli_abort("{.val {atac_mc}} does not contain RNA.")
     }
@@ -84,7 +85,7 @@ get_rna_matrix <- function(atac_mc, genes = NULL, rm_zeros = TRUE) {
             missing_genes <- genes[genes %!in% rownames(rna_mat)]
             cli_abort("Genes {.val {missing_genes}} are not a subset of {.field atac_mc@rna_egc}.")
         }
-        rna_mat <- rna_mat[genes, ]
+        rna_mat <- rna_mat[genes, , drop = FALSE]
     }
 
     if (rm_zeros) {
@@ -102,6 +103,143 @@ rm_zero_expr_genes <- function(rna_mat) {
     return(rna_mat)
 }
 
+
+#' Get normalized counts of gene expression
+#'
+#' Get counts of a gene expression normalized by sum of counts per metacell.
+#'
+#' @param epsilon regularization factor added to the log normalized expression
+#' @inheritParams get_rna_matrix
+#'
+#' @return a matrix with normalized counts of gene expression for each gene (rows) and metacell (columns)
+#'
+#'
+#' @examples
+#' \dontrun{
+#' get_rna_egc(atac_mc)
+#' get_rna_egc(atac_mc, "GNLY")
+#' get_rna_egc(atac_mc, "GNLY", epsilon = NULL)
+#' }
+#'
+#' @export
+get_rna_egc <- function(atac_mc, genes = NULL, rm_zeros = TRUE, epsilon = 1e-5) {
+    full_rna_mat <- get_rna_matrix(atac_mc, genes = NULL, rm_zeros = FALSE)
+    rna_mat <- get_rna_matrix(atac_mc, genes = genes, rm_zeros = rm_zeros)
+
+    res <- rna_mat / colSums(full_rna_mat)
+
+    if (!is.null(epsilon)) {
+        res <- res + epsilon
+    }
+
+    return(res)
+}
+
+#' Get enrichment of normalized counts of gene expression over the median
+#'
+#' @description
+#' The function first transforms the UMI matrix to fractions per metacell, and then calculates the enrichment of each gene
+#' over the median (dividing the gene expression by the median).
+#'
+#' @return a matrix with normalized counts of gene expression for each gene (rows) and metacell (columns)
+#'
+#' @inheritParams get_rna_egc
+#'
+#' @examples
+#' \dontrun{
+#' get_rna_fp(atac_mc)
+#' get_rna_fp(atac_mc, "GNLY")
+#' get_rna_fp(atac_mc, "GNLY", epsilon = NULL)
+#' }
+#'
+#' @export
+get_rna_fp <- function(atac_mc, genes = NULL, rm_zeros = TRUE, epsilon = 1e-5) {
+    rna_egc <- get_rna_egc(atac_mc, genes = genes, rm_zeros = rm_zeros, epsilon = epsilon)
+    rna_fp <- rna_egc / apply(rna_egc, 1, median, na.rm = TRUE)
+    return(rna_fp)
+}
+
+#' Calculate gene markers of metacells gene expression
+#'
+#' @description
+#' The function first removes genes without sufficient expression in any metacell \code{minimal_max_log_fraction}, or without sufficient fold
+#' change over the median (\code{minimal_relative_expression}), and then ranks the fold change of genes within each metacell. The markers
+#' are then the genes with the highest rank, up to \code{n_genes} genes.
+#'
+#' @param n_genes maximal number of genes to return. Default: 1000
+#' @param minimal_max_log_fraction take only genes with at least one value
+#' (in log fraction units - normalized egc) above this threshold
+#' @param minimal_relative_log_fraction take only genes with at least one value with relative
+#' log fraction (mc_fp) above this this value
+#' @param fold_change_reg regularization factor for the fold change calculation (fold_change would be changed to
+#' \code{fold_change = fold_change + fold_change_reg})
+#'
+#' @inheritParams get_rna_egc
+#'
+#' @examples
+#' \dontrun{
+#' get_rna_markers(atac_mc)
+#' }
+#'
+#' @export
+get_rna_markers <- function(atac_mc, n_genes = 200, genes = NULL, rm_zeros = TRUE, epsilon = 1e-5, minimal_max_log_fraction = -13, minimal_relative_log_fraction = 2, fold_change_reg = 0.1) {
+    mc_egc <- log2(get_rna_egc(atac_mc, genes = genes, rm_zeros = rm_zeros, epsilon = epsilon))
+
+    max_log_fractions_of_genes <- sparseMatrixStats::rowMaxs(mc_egc, na.rm = TRUE)
+    interesting_genes_mask <- max_log_fractions_of_genes >= minimal_max_log_fraction
+    cli_alert("removing {.val {sum(!interesting_genes_mask)}} genes with no RNA expression (log2) of above {.field {minimal_max_log_fraction}} in any metacell.")
+
+    mc_egc <- mc_egc[interesting_genes_mask, , drop = FALSE]
+    fold_matrix <- sweep(mc_egc, 1, sparseMatrixStats::rowMedians(mc_egc, na.rm = TRUE))
+    fold_matrix <- abs(fold_matrix + fold_change_reg)
+    max_relative_log_fractions_of_genes <- sparseMatrixStats::rowMaxs(fold_matrix, na.rm = TRUE)
+    interesting_genes_mask <- max_relative_log_fractions_of_genes >= minimal_relative_log_fraction
+    cli_alert("removing {.val {sum(!interesting_genes_mask)}} genes with no fold change (log2) of above {.field {minimal_relative_log_fraction}} in any metacell.")
+    fold_matrix[interesting_genes_mask, , drop = FALSE]
+
+    cli_alert_info("{.val {nrow(fold_matrix)}} genes left for consideration.")
+
+    if (nrow(fold_matrix) < n_genes) {
+        cli_abort("not enough genes left to return {.val {n_genes}} markers. Please try to set a lower value for {.field minimal_max_log_fraction} or {.field minimal_relative_log_fraction}.")
+    }
+
+    if (nrow(fold_matrix) > n_genes) {
+        gene_ranks <- sparseMatrixStats::colRanks(fold_matrix, useNames = TRUE, preserveShape = TRUE, ties.method = "first")
+        max_gene_ranks <- sparseMatrixStats::rowMaxs(gene_ranks, na.rm = TRUE, useNames = TRUE)
+        markers <- sort(max_gene_ranks, decreasing = TRUE) %>%
+            head(n = n_genes) %>%
+            names()
+    } else {
+        markers <- rownames(fold_matrix)
+    }
+
+    cli_alert_success("{.val {length(markers)}} marker genes selected.")
+
+    return(markers)
+}
+
+#' Get enrichment matrix for marker genes
+#'
+#' @param atac_mc a McATAC object with RNA expression (using \code{add_mc_rna})
+#' @param markers a list of marker genes. If NULL - the function uses \code{get_rna_markers} with default parameters which can be overridden
+#' using the ellipsis \code{...}.
+#' @inheritParams get_rna_fp
+#' @inheritDotParams get_rna_markers
+#'
+#' @examples
+#' \dontrun{
+#' marker_mat <- get_rna_marker_matrix(atac_mc)
+#' marker_mat <- get_rna_marker_matrix(atac_mc, n_genes = 100)
+#' }
+#'
+#' @export
+get_rna_marker_matrix <- function(atac_mc, markers = NULL, rm_zeros = TRUE, epsilon = 1e-5, ...) {
+    if (is.null(markers)) {
+        markers <- get_rna_markers(atac_mc, ...)
+    }
+    rna_fp <- get_rna_fp(atac_mc, genes = markers, rm_zeros = rm_zeros, epsilon = epsilon)
+    return(rna_fp)
+}
 
 #' Match every gene with the k ATAC peaks most correlated to it
 #'
