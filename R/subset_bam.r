ba1ae69a674a92fd89c3439289fade768285ee96
Yonatan Shapira <feshap@n76.mcl4.weizmann.ac.il>
2022-05-04 15:24:03 +0300

    added function to generate WIGs from BAMs

--- R/subset_bam.r
+++ R/subset_bam.r
@@ -25,25 +25,52 @@ generate_per_metacell_bams <- function(bam_path, mcatac, out_dir = NULL, c2mc_pa
     return(error_log)
 }
 
-#' Write tracks from BAMs
+#' Write WIGs from BAMs
 #' 
 #' @param bam_folder_path path to BAMs
 #' @param track_name_prefix general name for tracks (output format: "{track_name_prefix} - {bam_file_name}")
-generate_tracks_from_bams <- function(bam_folder_path, track_name_prefix) {
-    
-    header_line <- 'track type=wiggle_0 name="{track_name_prefix} - {}"'
-    command_format <- paste0(c("samtools mpileup -BQ0 {fl}", 
-                            "perl -pe '($c, $start, undef, $depth) = split;if ($c ne $lastC || $start != $lastStart+1)
-                                     {print \"fixedStep chrom=$c start=$start step=1 span=1\n\";}$_ = $depth.\"\n\";($lastC, $lastStart) = ($c, $start);'",
-                            "gzip -c > run.wig.gz"),
-                            collapse = " | ")
-    bams <- list.files(bam_folder_path)
-    dummy <- sapply(bams, function(fl) {
-        base_name <- gsub('.bam$', '', file.path(bam_folder_path, fl))
-        create_file_command <- glue::glue("echo ${header_line} > ")
-        system2()
-        system2()
-    })
+#' @param output_path (optional) where you want the WIG output
+#' @param parallel (optional) whether to do parallel computations
+generate_wigs_from_bams <- function(bam_folder_path, track_name_prefix, output_path = NULL, parallel = TRUE) {
+    bam_folder_path = normalizePath(bam_folder_path)
+    if (is.null(output_path)) {
+        output_path = file.path(bam_folder_path, "wig_output")
+    }
+    output_path = normalizePath(output_path)
+    if (!dir.exists(output_path)) {dir.create(output_path)}
+    header_line <- "track type=wiggle_0 name='{track_name_prefix} - {base_name}'"
+
+    # bam2wig_command <- paste0(paste0(c("samtools mpileup -BQ0 {fl}", 
+    #                         paste0("perl -pe '($c, $start, undef, $depth) = split;if ($c ne $lastC || $start != $lastStart+1) {print ", 
+    #                                 '"fixedStep chrom=$c start=$start step=1 span=1\n"',
+    #                                 ";\}$_ = $depth.\"\n\";($lastC, $lastStart) = ($c, $start);'"),
+    #                         collapse = " | "), 
+    #                         " >> {paste0(base_name, '.wig')}")
+    #                         )
+    bams <- grep('\\.bam$', list.files(bam_folder_path), v=T)
+    process_one_file = function(fl) {
+        print(fl)
+        base_name <- gsub('.bam$', '', fl)
+        full_path = file.path(output_path, paste0(base_name, '.wig'))
+        hli = glue::glue(header_line)
+        create_file_command <- glue::glue('echo "{hli}" > {full_path}')
+        system(command = create_file_command)
+        smt_cmd = glue::glue("samtools mpileup -BQ0 {file.path(bam_folder_path, fl)}")
+        perl_cmd = paste0("perl -pe '($c, $start, undef, $depth) = split;if ($c ne $lastC || $start != $lastStart+1) {print ", 
+                                    '"fixedStep chrom=$c start=$start step=1 span=1\n"',
+                                    ";}$_ = $depth.\"\n\";($lastC, $lastStart) = ($c, $start);'")
+        out_cmd = glue::glue(" >> {full_path}")
+        bam2wig_command = paste0(paste0(c(smt_cmd, perl_cmd), collapse = " | "), out_cmd)
+        system(command = bam2wig_command)
+    }
+    if (parallel) {
+        p = parallel::detectCores()
+        error_log <- parallel::mclapply(bams, function(fl) FUN = process_one_file(fl), mc.cores = p)
+    }
+    else {
+        error_log <- sapply(bams, function(fl) process_one_file(fl))
+    }
+    return(error_log)
 }
 
 
@@ -68,13 +95,18 @@ write_metacell_cell_names <- function(mcatac, c2mc_path = NULL) {
 #' @param bam_path path to the per-metacell BAM tracks
 #' @param output_filename what to call the output BAM file
 #' @param mcs vector of mcs to merge BAMs of
-merge_metacell_bams <- function(bam_path, output_filename, mcs) {
+#' @param parallel (optional) whether to do parallel computations
+merge_metacell_bams <- function(bam_path, output_filename, mcs, parallel = TRUE) {
     file_list <- paste(paste0(bam_path,'/mc',mcs, ".bam"))
     if (!grepl(".bam$", output_filename)) {
         output_filename <- paste0(output_filename, ".bam")
     }
     if (all(file.exists(file_list))) {
-        command <- glue::glue("samtools merge -f")
+        if (parallel) {
+            p = parallel::detectCores() - 1
+        }
+        else {p = 0}
+        command <- glue::glue("samtools merge -f --threads {p}")
         command <-paste(command, output_filename, paste0(file_list, collapse = ' '))
         system(command)
     }
