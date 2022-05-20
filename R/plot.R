d6eebc70b8f0d85b88bb30f86522d3659db9cf84
Yonatan Shapira <feshap@n102.mcl4.weizmann.ac.il>
2022-05-20 18:33:23 +0300

    added scale bar

--- R/plot.R
+++ R/plot.R
@@ -443,6 +443,7 @@ plot_tracks_at_locus <- function(tracks = NULL,
                                  annotation_col = NULL,
                                  annotation_colors = NULL,
                                  silent = FALSE,
+                                 scale_bar_length = NULL,
                                  rna_legc_eps = 1e-5,
                                  colors = colorRampPalette(c("white", "darkblue", "red"))(100),
                                  ...) {
@@ -551,6 +552,26 @@ plot_tracks_at_locus <- function(tracks = NULL,
     } else {
         peaks_ha <- NULL
     }
+    if (!is.null(scale_bar_length)) {
+        if (scale_bar_length > intervals$end - intervals$start) {
+            cli_alert_warning("Specified {.var scale_bar_length} is bigger than interval length. Ignoring.")
+            scale_bar_length <- 10**round(log10((intervals$end - intervals$start)/10))
+        } else if (scale_bar_length < iterator) {
+            cli_alert_warning("Specified {.var scale_bar_length} is shorter than {.var iterator}. Ignoring.")
+            scale_bar_length <- 10**round(log10((intervals$end - intervals$start)/10))
+        } 
+    } else {
+        scale_bar_length <- 10**round(log10((intervals$end - intervals$start)/10))
+    }
+    scale_bar_bins <- round(scale_bar_length/iterator)
+    sb_vec <- as.numeric(matrix(0,nrow=1,ncol(mat_n)))
+    sb_vec[(length(sb_vec)-scale_bar_bins):length(sb_vec)] <- 1
+    bottom_ha <- ComplexHeatmap::HeatmapAnnotation("scale_bar" = sb_vec,
+                                                which = "column",
+                                                show_legend = FALSE,
+                                                annotation_label = paste0(scale_bar_length, ' bp'),
+                                                col = list("scale_bar" = setNames(c('white', 'black'), c(0,1)))
+                                                )
     if (gene_annot) {
         gene_annots <- make_gene_annot(intervals, iterator, ncol(mat_n))
         if (!is.null(gene_annots[["labels"]]) && !is.null(gene_annots[["label_coords"]])) {
@@ -571,7 +592,8 @@ plot_tracks_at_locus <- function(tracks = NULL,
                 peaks = setNames(c("white", "red"), c(0, 1)),
                 genes = "black",
                 exons = setNames(c("white", "black"), c(0, 1))
-            )
+            ),
+            show_legend = FALSE
         )
     } else {
         gene_annots <- NULL
@@ -583,6 +605,7 @@ plot_tracks_at_locus <- function(tracks = NULL,
         top_annotation = top_ha,
         left_annotation = ct_ha,
         right_annotation = rna_ha,
+        bottom_annotation = bottom_ha,
         col = colors,
         cluster_rows = F,
         cluster_columns = F,
