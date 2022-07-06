21b6b7c3ea2188f03561c27d5de4394fd59f9cda
aviezerl <aviezer.lifshitz@weizmann.ac.il>
2022-07-06 18:39:31 +0300

    Added get_gene_promoter function

--- R/utils-intervals.R
+++ R/utils-intervals.R
@@ -122,3 +122,45 @@ gintervals.shift_right <- function(intervals, shift) {
         )
     return(intervals)
 }
+
+#' Get an intervals set of a gene promoter.
+#'
+#' @param name The name of the gene ("name2" in UCSC files).
+#' @param unify unify the intervals if there are multiple promoters. The start coordinate of the intervals will be the minimum of the starts and the end coordinate of the intervals will be the maximum of the ends.
+#' @inheritParams misha.ext::get_promoters
+#'
+#' @return an intervals set with the promoter of the gene. If \code{unify} is TRUE - the intervals will be unified to a single row.
+#'
+#' @examples
+#' \dontrun{
+#' get_gene_promoter("GZMK")
+#' }
+#'
+#' @export
+get_gene_promoter <- function(gene, upstream = 500, downstream = 50, unify = FALSE) {
+    if (!gintervals.exists("intervs.global.tss")) {
+        cli_abort("Intervals set named {.val intervs.global.tss} not found.")
+    }
+    promoters <- misha.ext::get_promoters(upstream = upstream, downstream = downstream)
+    if (!has_name(promoters, "geneSymbol")) {
+        cli_abort("Gene symbol not found in the {.val intervs.global.tss} intervals set.")
+    }
+    res <- promoters %>%
+        filter(geneSymbol == gene) %>%
+        select(chrom, start, end)
+
+    if (nrow(res) == 0) {
+        cli_abort("Gene {.val {gene}} not found in the {.val intervs.global.tss} intervals set.")
+    }
+
+    if (unify) {
+        res <- res %>%
+            summarise(
+                chrom = chrom[1],
+                start = min(start),
+                end = max(end)
+            )
+    }
+
+    return(res)
+}
