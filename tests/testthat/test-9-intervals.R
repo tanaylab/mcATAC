540141219f90267cdf958391a8f21c8c00ff8169
aviezerl <aviezer.lifshitz@weizmann.ac.il>
2022-07-07 11:22:57 +0300

    added extend_left and extend_right

--- tests/testthat/test-9-intervals.R
+++ tests/testthat/test-9-intervals.R
@@ -21,3 +21,15 @@ test_that("shift left and right works", {
     expect_equal(gintervals(1, 50, 200) %>% gintervals.shift_right(20), gintervals(1, 70, 220))
     expect_equal(gintervals(1, 0, 200) %>% gintervals.shift_right(20), gintervals(1, 20, 220))
 })
+
+test_that("extend left and right works", {
+    expect_equal(
+        gintervals(1, 30, 220),
+        gintervals(1, 50, 200) %>%
+            gintervals.extend_left(20) %>%
+            gintervals.extend_right(20)
+    )
+    expect_equal(gintervals(1, 50, 200) %>% gintervals.extend_left(20), gintervals(1, 30, 200))
+    expect_equal(gintervals(1, 50, 200) %>% gintervals.extend_right(20), gintervals(1, 50, 220))
+    expect_equal(gintervals(1, 0, 200) %>% gintervals.extend_left(20), gintervals(1, 0, 200))
+})
