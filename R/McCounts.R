267acce5aca54c35c900f8595dd14e60f8b3ba84
aviezerl <aviezer.lifshitz@weizmann.ac.il>
2023-04-25 16:08:27 +0300

    fix scc_to_mcc_multi_batch

--- R/McCounts.R
+++ R/McCounts.R
@@ -98,18 +98,25 @@ setMethod("Ops", c("McCounts", "McCounts"), function(e1, e2) {
 #' @export
 scc_to_mcc_multi_batch <- function(scc_dirs, cell_to_metacell, ignore_metacells = c(-1, -2)) {
     cell_to_metacell <- read_cell_to_metacell(cell_to_metacell, ignore_metacells)
-    mcc_list <- purrr::map(scc_dirs, ~ {
-        if (!dir.exists(.x)) {
+    
+    add_scc <- function(dir, x = NULL){   
+        if (!dir.exists(dir)) {
             cli_abort("Directory {.x} does not exist")
-        }
-        scc <- scc_read(.x)
+        }     
+        scc <- scc_read(dir)
         cell_to_metacell_batch <- cell_to_metacell %>%
             filter(cell_id %in% scc@cell_names)
-        scc_to_mcc(scc, cell_to_metacell_batch, ignore_metacells)
-    })
+        mcc1 <- scc_to_mcc(scc, cell_to_metacell_batch, ignore_metacells)
+        if (!is.null(x)){
+            return(x + mcc1)    
+        }
+        return(mcc1)        
+    }
 
-    cli_alert("Merging metacells from multiple batches")
-    mcc <- Reduce("+", mcc_list)
+    mcc <- NULL
+    for (dir in scc_dirs){
+        mcc <- add_scc(dir, mcc)        
+    }
     return(mcc)
 }
 
