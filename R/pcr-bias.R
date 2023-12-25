8708c414a98b2ae7c4ef9ff3c095a709f93db4e0
aviezerl <aviezer.lifshitz@weizmann.ac.il>
2023-12-25 14:10:40 +0200

    fixed 'smoo' instead of 'smoothed'

--- R/pcr-bias.R
+++ R/pcr-bias.R
@@ -26,7 +26,7 @@
 normalize_marginal <- function(orig_track, norm_track, smoothed_track = NULL, window_size = 20e3, iterator = 1e3, floor_percentile = 0.1, overwrite = FALSE) {
     if (is.null(smoothed_track)) {
         gdir.create("temp", showWarnings = FALSE)
-        smoo_track <- temp_track_name("temp.")
+        smoothed_track <- temp_track_name("temp.")
     }
 
     if (gtrack.exists(norm_track)) {
@@ -38,10 +38,19 @@ normalize_marginal <- function(orig_track, norm_track, smoothed_track = NULL, wi
         }
     }
 
+    if (gtrack.exists(smoothed_track)) {
+        if (overwrite) {
+            cli_alert_warning("Removing previous track {.val {smoothed_track}}")
+            gtrack.rm(smoothed_track, force = TRUE)
+        } else {
+            cli_abort("{smoothed_track} already exists. Use 'overwrite = TRUE' to overwrite.")
+        }
+    }
+
     cli_alert_info("Smoothing track {.val {orig_track}} with window size {.val {window_size}} and iterator {.val {iterator}}")
     # Create a track of smoothed ATAC signal (geometric mean of window_size windows)
     gtrack.smooth(
-        track = smoo_track,
+        track = smoothed_track,
         description = glue("Smoothed track of {orig_track} with window size {window_size} and iterator {iterator}"),
         expr = glue("log2(1 + ifelse(is.na({orig_track}), 0, {orig_track}))"),
         winsize = window_size,
@@ -52,12 +61,12 @@ normalize_marginal <- function(orig_track, norm_track, smoothed_track = NULL, wi
     )
 
     # we set the minimal value to be the floor_percentile of the smoothed track (and not less than 1)
-    floor_val <- gquantiles(smoo_track, floor_percentile)
+    floor_val <- gquantiles(smoothed_track, floor_percentile)
 
     # We now normalize the marginal track by the smoothed track.
     # The norm is in log-scale of the value + 1 (geometric mean) hence the exponent of 2 and the -1
     cli_alert_info("Normalizing track {.val {orig_track}} to track {.val {norm_track}}")
-    expr <- glue("{orig_track} / 2^({smoo_track_floored} - 1)", smoo_track_floored = glue("pmax(1, 1 + ifelse(is.na({smoo_track}), 0, {smoo_track} - {floor_val}))"))
+    expr <- glue("{orig_track} / 2^({smoo_track_floored} - 1)", smoo_track_floored = glue("pmax(1, 1 + ifelse(is.na({smoothed_track}), 0, {smoothed_track} - {floor_val}))"))
     orig_description <- gtrack.attr.export(orig_track)$description
     description <- glue("{orig_description} (normalized by a geometric mean of {window_size}bp windows and iterator of {iterator})")
     gtrack.create(
