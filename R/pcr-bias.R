d14894703221d0e92afafebc25bd8567fc2de692
aviezerl <aviezer.lifshitz@weizmann.ac.il>
2024-01-28 15:54:31 +0200

    Added signal normalization functions

--- R/pcr-bias.R
+++ R/pcr-bias.R
@@ -116,10 +116,156 @@ normalize_egc <- function(mcatac, marginal_track, window_size = 1e4, epsilon = 1
         mutate(norm_f = 1 / norm_f)
     mc_mat_s <- mcatac@mat * peaks_metadata$norm_f
     mc_egc_s <- t(t(mc_mat_s) / colSums(mc_mat_s))
-    
+
     mcatac@egc <- mc_egc_s
     mcatac@fp <- calc_mc_fp(mcatac)
 
     return(mcatac)
 }
 
+
+#' Normalize McPeaks data using constitutive peaks
+#'
+#' The function normalizes the McPeaks data using the values of the mean of constitutive peaks in a specified cell type. The normalized data is then scaled to a specified quantile and capped at 1.
+#'
+#' @param mcatac The McPeaks object containing the data to be normalized. The object should contain a field named {.field const} in {.field peaks}. Use \code{add_const_peaks} to add this field.
+#' @param norm_type The cell type to use as the reference for normalization
+#' @param norm_quant The quantile to which the normalized data should be scaled (default is 1).
+#'
+#' @return The McPeaks object with normalized egc values.
+#'
+#' @examples
+#' \dontrun{
+#' mcatac <- import_from_matrix(mat, peaks, genome = "mm10", class = "McPeaks")
+#' mcatac <- normalize_egc(mcatac, "gastrulation.marginal")
+#' mcatac <- add_const_peaks(mcatac, -16)
+#' mcatac <- normalize_const(mcatac, norm_type = "Epiblast")
+#' }
+#'
+#' @export
+normalize_const <- function(mcatac, norm_type, norm_quant = 1) {
+    if (!has_name(mcatac@peaks, "const")) {
+        cli_abort("No field named {.field const} in peaks. Please run {.code add_const_peaks}")
+    }
+
+    const_peaks <- mcatac@peaks$peak_name[mcatac@peaks$const]
+    egc <- mcatac@egc
+
+    if (!has_cell_type(mcatac)) {
+        cli_abort("No cell_type in metadata")
+    }
+
+    if (!(norm_type %in% mcatac@metadata$cell_type)) {
+        cli_abort("norm_type {.val {norm_type}} is not in {.field metadata$cell_type}")
+    }
+
+    norm_type_mcs <- mcatac@metadata$metacell[mcatac@metadata$cell_type == norm_type]
+
+    const_peak_score_all <- colSums(egc[const_peaks, ])
+
+    const_peak_score_norm_type <- mean(const_peak_score_all[norm_type_mcs])
+    const_peak_score_norm <- const_peak_score_all / const_peak_score_norm_type
+
+    egc_norm <- t(t(egc) / const_peak_score_norm)
+    egc_norm <- egc_norm / quantile(egc_norm, norm_quant)
+    egc_norm[egc_norm > 1] <- 1
+
+    egc_norm <- egc_norm * max(egc[, norm_type_mcs])
+
+    mcatac@egc <- egc_norm
+
+    return(mcatac)
+}
+
+
+
+
+#' Calculate constitutive peaks
+#'
+#' This function calculates the constitutive peaks based on the minimal expression of each peak across different cell types.
+#'
+#' @param mcatac McPeaks object.
+#' @param const_threshold The threshold value for determining constitutive peaks. A peak would be considered constitutive if its minimal expression across all cell types is greater than or equal to this threshold.
+#'
+#' @return A named logical vector indicating whether each peak is constitutive or not.
+#'
+#' @examples
+#' \dontrun{
+#' mcatac <- import_from_matrix(mat, peaks, genome = "mm10", class = "McPeaks")
+#' mcatac <- normalize_egc(mcatac, "gastrulation.marginal")
+#' const_v <- calc_const_peaks(mcatac, -16)
+#' }
+#'
+#' @export
+calc_const_peaks <- function(mcatac, const_threshold) {
+    mc_egc <- mcatac@egc
+    mc_egc <- log2(mc_egc + 1e-5)
+
+    if (!has_cell_type(mcatac)) {
+        cli_abort("No cell_type in metadata")
+    }
+
+    # get cell type egc average for each peak
+    ct_egc <- t(tgs_matrix_tapply(mc_egc, mcatac@metadata$cell_type, mean, na.rm = TRUE))
+
+    const_v <- tibble(cre = rownames(ct_egc), const = apply(ct_egc, 1, min, na.rm = TRUE) >= const_threshold) %>%
+        select(cre, const) %>%
+        deframe()
+
+    return(const_v)
+}
+
+#' Add constitutive peaks to McPeaks object
+#'
+#' This function adds a field named {.field const} to the {.field peaks} field of the McPeaks object. The field contains a named logical vector indicating whether each peak is constitutive or not.
+#'
+#' @return The McPeaks object with the added field.
+#'
+#' @examples
+#' \dontrun{
+#' mcatac <- import_from_matrix(mat, peaks, genome = "mm10", class = "McPeaks")
+#' mcatac <- normalize_egc(mcatac, "gastrulation.marginal") #'
+#' mcatac <- add_const_peaks(mcatac, -16)
+#' }
+#'
+#' @inheritParams calc_const_peaks
+#' @export
+add_const_peaks <- function(mcatac, const_threshold6) {
+    mcatac@peaks$const <- calc_const_peaks(mcatac, const_threshold)
+    return(mcatac)
+}
+
+#' Normalize the mcATAC object to probability values
+#'
+#' This function normalizes the mcATAC object to probability values by setting values above a threshold to the threshold value,
+#' and then scaling the values to the range [0, 1].
+#'
+#' @param mcatac The mcPeaks object to be normalized.
+#' @param prob1_thresh The threshold value for setting values above it to the threshold value. If not provided, the function will calculate the threshold using the "const" field in the peaks.
+#' @param const_quantile The quantile value used to calculate the threshold when "prob1_thresh" is not provided.
+#'
+#' @return The normalized mcATAC object.
+#'
+#' @examples
+#' \dontrun{
+#' mcatac <- import_from_matrix(mat, peaks, genome = "mm10", class = "McPeaks")
+#' mcatac <- normalize_egc(mcatac, "gastrulation.marginal")
+#' mcatac <- add_const_peaks(mcatac, -16)
+#' mcatac <- normalize_to_prob(mcatac)
+#' }
+#'
+#' @export
+normalize_to_prob <- function(mcatac, prob1_thresh = NULL, const_quantile = 0.8) {
+    if (is.null(prob1_thresh)) {
+        if (!has_name(mcatac@peaks, "const")) {
+            cli_abort("No field named {.field const} in peaks. Please run {.code add_const_peaks}")
+        }
+        prob1_thresh <- quantile(mcatac@egc[mcatac@peaks$const, ], const_quantile)
+        cli::cli_alert("Using {.val {prob1_thresh}} as the p=1 threshold")
+    }
+
+    mcatac@egc[mcatac@egc > prob1_thresh] <- prob1_thresh
+    mcatac@egc <- norm01(mcatac@egc)
+
+    return(mcatac)
+}
